5c4f989b6f1bb1f7c8f6a00c98f5e95a8adbd4a1
oriol.llorens.7e9 <oriol.llorens.7e9@itb.cat>
2026-02-05 13:04:08 +0100

    ll

---
+++ POO/JS/ej1.js
@@ -0,0 +1,16 @@
+class TarjetaCredito{
+    numero;
+    titular;
+    ccv;
+    fechaCaducidad; 
+    pin;
+
+    constructor () {
+        this.numero = ;
+        this.titular = "Oriol Llorens Ramirez";
+        this.ccv = 343
+        this.fechaCaducidad = 5/34
+        this.pin =
+
+    }
+}
